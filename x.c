2173212a09303c29d8a2d57a8c4ad3e4324bd961
ziro <palembani@gmail.com>
2020-05-09 10:11:42 +0700

    + Added keyboard_select & fixed copy paste

--- x.c
+++ x.c
@@ -1819,6 +1819,12 @@ kpress(XEvent *ev)
 		len = XmbLookupString(xw.ime.xic, e, buf, sizeof buf, &ksym, &status);
 	else
 		len = XLookupString(e, buf, sizeof buf, &ksym, NULL);
+	if ( IS_SET(MODE_KBDSELECT) ) {
+		if ( match(XK_NO_MOD, e->state) || 
+		(XK_Shift_L | XK_Shift_R) & e->state )
+		win.mode ^= trt_kbdselect(ksym, buf, len);
+		return;
+    }
 	/* 1. shortcuts */
 	for (bp = shortcuts; bp < shortcuts + LEN(shortcuts); bp++) {
 		if (ksym == bp->keysym && match(bp->mod, e->state)) {
@@ -2003,6 +2009,14 @@ usage(void)
 }
 
 
+
+void toggle_winmode(int flag) {
+        win.mode ^= flag;
+}
+
+void keyboard_select(const Arg *dummy) {
+    win.mode ^= trt_kbdselect(-1, NULL, 0);
+}
 
 int
 main(int argc, char *argv[])
