dfbdafc19fb2af462308a77739636903e8dc1bb4
ziro <palembani@gmail.com>
2020-05-09 12:24:16 +0700

    + Added font2 patch for Nerd Font

--- config.h
+++ config.h
@@ -6,6 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Iosevka:size=10:antialias=true:autohint=true";
+static char *font2[] = { "UbuntuMono Nerd Font:size=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
